71a58af880341c4ba4647ee538e264b3c90cf782
muratyildizz <yildizmurat42953@gmail.com>
2023-12-19 02:59:57 +0300

    gffı

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
-        //murat
+        //murat yıldız
     }
 }
