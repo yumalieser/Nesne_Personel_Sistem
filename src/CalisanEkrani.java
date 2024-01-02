64526402f99df1b75152236666a19666fa9b5da8
Furkanyolcu <119127899+Furkanyolcu@users.noreply.github.com>
2024-01-02 21:12:25 +0300

    .

--- src/CalisanEkrani.java
+++ src/CalisanEkrani.java
@@ -352,20 +352,7 @@ public class CalisanEkrani extends javax.swing.JDialog {
         
         
     }
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                CalisanEkrani dialog = new CalisanEkrani(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
-    }
+
 
     private javax.swing.JTextField ad_alani;
     private javax.swing.JTextField arama_cubugu;
